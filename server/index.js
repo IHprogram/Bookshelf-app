cbf329bd67030c759f74a3a5ca363674b34ffc14
IHprogram <ihm20202020@gmail.com>
2021-07-13 10:32:38 +0900

    サーバーのポート番号を変更

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import mongoURI from './mongouri.js';
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
