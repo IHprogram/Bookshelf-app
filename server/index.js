dc1c282fba5ac0fc6d78a2e4ea94e716350fd8b3
IHprogram <ihm20202020@gmail.com>
2021-07-13 17:01:52 +0900

    Expressで作成したサーバーにルーティングの読み込みを追加

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from "mongoose";
 import cors from 'cors';
 import mongoURI from './mongouri.js';
+import bookRoutes from './routes/books.js';
 
 const app = express();
 const port = 3002;
@@ -9,6 +10,8 @@ const port = 3002;
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
+
+app.use('/books', bookRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
