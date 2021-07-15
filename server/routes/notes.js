bb3744b02350f271978423b0cbd1d474e0f1a892
IHprogram <ihm20202020@gmail.com>
2021-07-15 12:10:45 +0900

    ノートを一覧表示するためのルーティングを設定

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createNote } from '../controllers/notes.js';
+import { getNotes, createNote } from '../controllers/notes.js';
 
 const router = express.Router();
 
+router.get('/', getNotes)
 router.post('/', createNote);
 
 export default router;
