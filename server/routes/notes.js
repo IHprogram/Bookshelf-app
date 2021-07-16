fbf203324f873c07abef88711fc70820f8b40dfd
IHprogram <ihm20202020@gmail.com>
2021-07-16 16:36:43 +0900

    ノートの編集を行うためのルーティングを設定

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getNotes, createNote, deleteNote } from '../controllers/notes.js';
+import { getNotes, createNote, updateNote, deleteNote } from '../controllers/notes.js';
 
 const router = express.Router();
 
 router.get('/', getNotes)
 router.post('/', createNote);
+router.put('/:id', updateNote);
 router.delete('/:id', deleteNote);
 
 export default router;
