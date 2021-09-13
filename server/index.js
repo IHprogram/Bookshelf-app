b57857a211bded565d5c8abc7a414578249f9da4
IHprogram <ihm20202020@gmail.com>
2021-09-14 06:14:16 +0900

    process.env.PORTの追加

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import bookRoutes from './routes/books.js';
 import noteRoutes from './routes/notes.js';
 
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
